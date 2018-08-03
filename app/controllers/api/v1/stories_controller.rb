9dc26da753daca6d96b0be91a4dba1c87ea8babd
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-08-03 12:03:40 +0800

    update

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -1,7 +1,34 @@
 # app/controllers/api/v1/stories_controller.rb 
 class Api::V1::StoriesController < Api::V1::BaseController 
+    before_action :set_story, only: [ :show, :update ]
     def index 
         @stories = Story.all 
         # render json: @stories
     end 
+
+    def show
+    end
+
+    def update
+        if @story.update(story_params)
+            render :show
+        else
+            render_error
+        end
+    end
+
+    private
+
+    def set_story
+        @story = Story.find(params[:id])
+        # render json: @story
+    end
+
+    def story_params 
+		params.require(:story).permit(:name, :text)
+	 end 
+	
+	def render_error 
+		render json: { errors: @story.errors.full_messages }, status: :unprocessable_entity 
+	end
 end
